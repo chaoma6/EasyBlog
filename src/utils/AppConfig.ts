bd391082ff5b9edc17bcfad9be6ece88fbb7f3f9
Chao Ma <machaosydney@gmail.com>
2023-06-10 16:19:21 +1000

    docs(update app configuration): update app configuration

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,9 @@
 // FIXME: Update this configuration file based on your project information
 
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'EasyBlog',
+  title: 'Nextjs EasyBlog',
+  description:
+    'Experience the ease and style of blogging with Next.js 13 and Headless CMS',
   locale: 'en',
 };
