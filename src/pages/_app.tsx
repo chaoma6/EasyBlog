dc90035e562c766818aed46a232be51374ba5573
Chao Ma <machaosydney@gmail.com>
2023-06-20 19:22:43 +1000

    feat(add google auth): add google auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 /* eslint-disable */
+import { SessionProvider } from 'next-auth/react';
 import '../styles/global.css';
 
 import type { AppProps } from 'next/app';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+const MyApp = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) => (
+  <SessionProvider session={session}>
+    <Component {...pageProps} />
+  </SessionProvider>
 );
 
 export default MyApp;
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -3,6 +3,7 @@
 /* eslint-disable jsx-a11y/heading-has-content */
 /* eslint-disable react/no-unstable-nested-components */
 import type { GetStaticProps } from 'next';
+import { useSession } from 'next-auth/react';
 import React, { useState } from 'react';
 import type { SubmitHandler } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
@@ -26,7 +27,9 @@ type Inputs = {
 };
 
 const PostPage = ({ post }: Props) => {
+  const { data: session } = useSession();
   const [submitted, setSubmitted] = useState(false);
+  const [userErr, setuserErr] = useState('');
   const {
     register,
     handleSubmit,
@@ -39,7 +42,7 @@ const PostPage = ({ post }: Props) => {
       body: JSON.stringify(data),
     })
       .then(() => setSubmitted(true))
-      .catch((err) => setSubmitted(false));
+      .catch((_) => setSubmitted(false));
   };
 
   const formattedPublishedAt = new Date(post.publishedAt).toLocaleDateString(
@@ -50,6 +53,14 @@ const PostPage = ({ post }: Props) => {
       year: 'numeric',
     }
   );
+
+  const handleUserErr = () => {
+    if (!session) {
+      setuserErr('Please sign in to comment!');
+    } else {
+      setuserErr('');
+    }
+  };
 
   return (
     <div>
@@ -120,98 +131,127 @@ const PostPage = ({ post }: Props) => {
           </div>
         </article>
         <hr className="mx-auto my-5 max-w-lg border-[1px] border-secondaryColor" />
-        <div>
-          <p className="font-titleFont text-xs font-bold uppercase text-secondaryColor">
-            Enjoyed this article?
-          </p>
-          <h3 className="font-titleFont text-3xl font-bold">
-            Leave a comment below!
-          </h3>
-          <hr className="mt-2 py-3" />
-          {/* Form */}
-          {/* {Generating Id for hooks form} */}
-          <input
-            {...register('_id')}
-            type="hidden"
-            name="_id"
-            value={post._id}
-          />
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="mt-7 flex flex-col gap-6"
-          >
-            <label className="flex flex-col" htmlFor="nameInput">
-              <span className="font-titleFont text-base font-semibold">
-                Name
-              </span>
-              <input
-                {...register('name', { required: true })}
-                id="nameInput"
-                type="text"
-                className="border-b-[1px] border-secondaryColor px-4 py-1 text-base shadow-secondaryColor outline-none placeholder:text-sm focus-within:shadow-xl"
-                placeholder="Enter your name"
-              />
-              {errors.name && (
-                <span className="text-sm text-red-500">
-                  This field is required
-                </span>
-              )}
-            </label>
-            <label className="flex flex-col" htmlFor="emailInput">
-              <span className="font-titleFont text-base font-semibold">
-                Email
-              </span>
-              <input
-                {...register('email', { required: true })}
-                id="emailInput"
-                type="email"
-                className="border-b-[1px] border-secondaryColor px-4 py-1 text-base shadow-secondaryColor outline-none placeholder:text-sm focus-within:shadow-xl"
-                placeholder="Enter your Email"
-              />
-              {errors.email && (
-                <span className="text-sm text-red-500">
-                  This field is required
-                </span>
-              )}
-            </label>
-            <label className="flex flex-col" htmlFor="textareaInput">
-              <span className="font-titleFont text-base font-semibold">
-                Comment
-              </span>
-              <textarea
-                {...register('comment', { required: true })}
-                id="textareaInput"
-                className="border-b-[1px] border-secondaryColor px-4 py-1 text-base shadow-secondaryColor outline-none placeholder:text-sm focus-within:shadow-xl"
-                placeholder="Enter your comments"
-                rows={6}
-              />
-              {errors.comment && (
-                <span className="text-sm text-red-500">
-                  This field is required
-                </span>
-              )}
-            </label>
-            <button
-              type="submit"
-              className="w-full rounded-sm bg-bgColor py-2 font-titleFont text-base font-semibold uppercase tracking-wider text-white duration-300 hover:bg-secondaryColor"
-            >
-              Submit
-            </button>
-          </form>
-          {/* {comments} */}
-          <div className="mx-auto my-10 flex w-full flex-col space-y-2 p-10 shadow-lg shadow-bgColor">
-            <h3 className="font-titleFont text-3xl font-semibold">Comments</h3>
-            <hr />
-            {post.comments.map((comment) => (
-              <div key={comment._id}>
-                <p>
-                  <span className="text-secondaryColor"> {comment.name}</span>{' '}
-                  {comment.comment}
-                </p>
-              </div>
-            ))}
+        {submitted ? (
+          <div className="mx-auto my-10 flex flex-col items-center gap-2 bg-bgColor p-10 text-white">
+            <h1 className="text-2xl font-bold">
+              Thank you for submitting your comment!
+            </h1>
+            <p>Once it has been approved, it will appear below!</p>
           </div>
-        </div>
+        ) : (
+          <div>
+            <p className="font-titleFont text-xs font-bold uppercase text-secondaryColor">
+              Enjoyed this article?
+            </p>
+            <h3 className="font-titleFont text-3xl font-bold">
+              Leave a comment below!
+            </h3>
+            <hr className="mt-2 py-3" />
+            {/* Form */}
+            {/* Generating Id for hooks form */}
+            <input
+              {...register('_id')}
+              type="hidden"
+              name="_id"
+              value={post._id}
+            />
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="mt-7 flex flex-col gap-6"
+            >
+              <label className="flex flex-col" htmlFor="nameInput">
+                <span className="font-titleFont text-base font-semibold">
+                  Name
+                </span>
+                <input
+                  {...register('name', { required: true })}
+                  id="nameInput"
+                  type="text"
+                  className="border-b-[1px] border-secondaryColor px-4 py-1 text-base shadow-secondaryColor outline-none placeholder:text-sm focus-within:shadow-xl"
+                  placeholder="Enter your name"
+                />
+                {errors.name && (
+                  <span className="text-sm text-red-500">
+                    This field is required
+                  </span>
+                )}
+              </label>
+              <label className="flex flex-col" htmlFor="emailInput">
+                <span className="font-titleFont text-base font-semibold">
+                  Email
+                </span>
+                <input
+                  {...register('email', { required: true })}
+                  id="emailInput"
+                  type="email"
+                  className="border-b-[1px] border-secondaryColor px-4 py-1 text-base shadow-secondaryColor outline-none placeholder:text-sm focus-within:shadow-xl"
+                  placeholder="Enter your Email"
+                />
+                {errors.email && (
+                  <span className="text-sm text-red-500">
+                    This field is required
+                  </span>
+                )}
+              </label>
+              <label className="flex flex-col" htmlFor="textareaInput">
+                <span className="font-titleFont text-base font-semibold">
+                  Comment
+                </span>
+                <textarea
+                  {...register('comment', { required: true })}
+                  id="textareaInput"
+                  className="border-b-[1px] border-secondaryColor px-4 py-1 text-base shadow-secondaryColor outline-none placeholder:text-sm focus-within:shadow-xl"
+                  placeholder="Enter your comments"
+                  rows={6}
+                />
+                {errors.comment && (
+                  <span className="text-sm text-red-500">
+                    This field is required
+                  </span>
+                )}
+              </label>
+              {session && (
+                <button
+                  type="submit"
+                  className="w-full rounded-sm bg-bgColor py-2 font-titleFont text-base font-semibold uppercase tracking-wider text-white duration-300 hover:bg-secondaryColor"
+                >
+                  Submit
+                </button>
+              )}
+            </form>
+            {!session && (
+              <button
+                onClick={handleUserErr}
+                type="button"
+                className="w-full rounded-sm bg-bgColor py-2 font-titleFont text-base font-semibold uppercase tracking-wider text-white duration-300 hover:bg-secondaryColor"
+              >
+                Submit
+              </button>
+            )}
+            {userErr && (
+              <p className="my-1 animate-bounce px-4 text-center font-titleFont text-sm font-semibold text-red-500 underline underline-offset-2">
+                {' '}
+                <span className="mr-2 text-base font-bold italic">!</span>
+                {userErr}
+              </p>
+            )}
+            {/* {comments} */}
+            <div className="mx-auto my-10 flex w-full flex-col space-y-2 p-10 shadow-lg shadow-bgColor">
+              <h3 className="font-titleFont text-3xl font-semibold">
+                Comments
+              </h3>
+              <hr />
+              {post.comments.map((comment) => (
+                <div key={comment._id}>
+                  <p>
+                    <span className="text-secondaryColor"> {comment.name}</span>{' '}
+                    {comment.comment}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
       <Footer />
     </div>
@@ -250,7 +290,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
       name,
       image
     },
-    "comments":[_type == "comment" && post._ref == ^._id && approved == true],
+    "comments":*[_type == "comment" && post._ref == ^._id && approved == true],
     description,
     mainImage,
     slug,
