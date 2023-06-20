7f07ab7977460a18b6d4a54f7699482d3a4c08f5
Chao Ma <machaosydney@gmail.com>
2023-06-20 20:28:29 +1000

    fix(add apiVersion): add apiVersion

--- sanity.js
+++ sanity.js
@@ -4,8 +4,8 @@ import { createClient } from 'next-sanity';
 import createImageUrlBuilder from '@sanity/image-url';
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'rzwcrflf';
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2022-11-16';
 
 // eslint-disable-next-line import/prefer-default-export
 export const config = {
